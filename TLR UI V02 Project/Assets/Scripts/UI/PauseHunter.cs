060352b59331a28f6fa2fd244b8519b875a453b1
pkcody <77869251+pkcody@users.noreply.github.com>
2022-12-01 17:03:40 -0700

    functional with controller

---
+++ TLR UI V02 Project/Assets/Scripts/UI/PauseHunter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseHunter : MonoBehaviour
+{
+    private void Start()
+    {
+        pauseHunt();
+    }
+
+
+    public void pauseHunt()
+    {
+        foreach (PauseGameMenu pgm in FindObjectsOfType<PauseGameMenu>())
+        {
+            pgm.pauseGameMenu = gameObject;
+        }
+        if(SceneManager.GetActiveScene().name != "MainMenu")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
