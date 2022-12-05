d7132003a3aa38f7b8d1879304ffcb83ac5bce7d
pkcody <77869251+pkcody@users.noreply.github.com>
2022-12-05 03:39:05 -0700

    more ui after crash

--- TLR UI V02 Project/Assets/Scripts/Multiplayer/PlayerSpawning.cs
+++ TLR UI V02 Project/Assets/Scripts/Multiplayer/PlayerSpawning.cs
@@ -10,11 +10,15 @@ public class PlayerSpawning : MonoBehaviour
     public static PlayerSpawning instance;
 
     public GameObject[] players = new GameObject[4];
+    public Transform[] hoverInfo = new Transform[4];
     public Material[] mats = new Material[4];
     //public InventoryObject[] inv = new InventoryObject[4];
 
+    public Transform[] JoinSpawnPos = new Transform[4];
     public Transform[] MenuSpawnPos = new Transform[4];
     public Transform[] GameSpawnPos = new Transform[4];
+
+    public bool lockedIn = false;
 
     private void Awake()
     {
@@ -39,7 +43,13 @@ public class PlayerSpawning : MonoBehaviour
                 int Index = System.Array.IndexOf(players, go);
 
                 // Different spawns for each scene
-                if (SceneManager.GetActiveScene().name == "MainMenu")
+                if (SceneManager.GetActiveScene().name == "Join")
+                {
+                    go.GetComponent<PlayerInput>().SwitchCurrentActionMap("UI");
+                    print("calling");
+                    go.GetComponent<PlayerInput>().defaultActionMap = "UI";
+                }
+                else if (SceneManager.GetActiveScene().name == "MainMenu")
                 {
                     go.GetComponent<PlayerInput>().SwitchCurrentActionMap("UI");
                     print("calling");
@@ -85,11 +95,34 @@ public class PlayerSpawning : MonoBehaviour
             if (go != null)
             {
                 int Index = System.Array.IndexOf(players, go);
-                go.transform.position = MenuSpawnPos[Index].position;
+                go.transform.position = JoinSpawnPos[Index].position;
 
                 go.transform.eulerAngles = Vector3.zero;
                 go.transform.GetChild(0).GetComponent<MeshRenderer>().material = mats[Index];
                 //go.GetComponent<CharacterMovement>().inventory = inv[Index];
+
+                
+            }
+
+            //in future change to switch
+            foreach (Transform goih in hoverInfo)
+            {
+                if (go == null)
+                {
+                    goih.GetChild(1).gameObject.SetActive(true);
+                    goih.GetChild(2).gameObject.SetActive(false);
+
+                }
+                else if (go != null && !lockedIn)
+                {
+                    goih.GetChild(1).gameObject.SetActive(false);
+                    goih.GetChild(2).gameObject.SetActive(true);
+
+                }
+                else if (lockedIn)
+                {
+                    goih.GetChild(0).gameObject.SetActive(false);
+                }
             }
         }
         //FindObjectOfType<PauseHunter>().pauseHunt();
